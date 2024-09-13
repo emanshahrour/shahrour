33f131de231a7f720f94cc06906653e40d353fe9
emanshahrour <poststelle@eman-shahrour.de>
2024-09-13 11:52:20 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,14 @@
-var mapFrame = document.getElementById('map-frame');
-mapFrame.addEventListener('click', function() {
-    mapFrame.style.pointerEvents = 'auto';
-});
+function initMap() {
+  var location = { lat: 53.56072645866264, lng: 8.573987175677178 };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: location
+  });
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Eman Hussein Shahrour'
+  });
+}
